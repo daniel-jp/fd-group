07138d076a25017ac1950e5a51b7951d50b52451
Daniel-jp <danny198672@hotmail.com>
2023-02-19 17:39:59 +0100

    Links in NavBar3

--- src/components/Home/FdTestimo.tsx
+++ src/components/Home/FdTestimo.tsx
@@ -115,7 +115,7 @@ function FdTestimo() {
               </TestimonialText>
             </TestimonialContent>
             <TestimonialAvatar
-              src={"../../public/assets/testmo/avatar.png"}
+              src={"https://media.licdn.com/dms/image/D4E03AQGvBkytqkkSPw/profile-displayphoto-shrink_100_100/0/1664478662979?e=1682553600&v=beta&t=sRF5-sZH2SAN16-4ttH3EhWxM0Yqii5crOCW7qkXGkw"}
               name={"Daniel Paulino"}
               title={t("Title1")} />
           </Testimonial>
